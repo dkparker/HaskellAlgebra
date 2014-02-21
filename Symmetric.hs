8870da95140dc1b574ef02f26af4e66382d541ac
Daniel Parker <dkparker@uchicago.edu>
2014-02-20 20:23:25 -0600

    Fixed Symmetric to be associative as it should be, made definition of composition clearer.

--- Symmetric.hs
+++ Symmetric.hs
@@ -6,10 +6,13 @@ import Data.List
 data Perm = Perm [Int]
     deriving(Show, Eq)
 
-compose :: Perm -> Perm -> Perm
-compose (Perm as) (Perm bs) 
-    |length as == length bs && length as == 6 && sort as == sort bs = Perm $ map fst $ reverse $ sortBy (\b -> \a -> compare (snd a) (snd b)) $ zipWith (,) as bs
+apply :: Perm -> Perm -> Perm
+apply (Perm as) (Perm bs) 
+    |length as == length bs && length as == 6 && sort as == sort bs = Perm $ map fst $ reverse $ sortBy (\y -> \x -> compare (snd x) (snd y)) $ zipWith (,) as bs
     |otherwise = Perm []
+
+compose :: Perm -> Perm -> Perm
+compose (Perm as) (Perm bs) = apply (apply (Perm [1,2,3,4,5,6]) (Perm bs)) (Perm as)
 
 instance Group Perm where
     gbinop = compose
