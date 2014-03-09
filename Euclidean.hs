72bdf1cf08d311b0e650f1937ab1724795ec5622
Daniel Parker <dkparker@uchicago.edu>
2014-03-09 17:07:59 -0500

    Added Euclidean, containing polynomials with addition and multiplication as an instance of Ring. Still need to add euclidean function and division.

---
+++ Euclidean.hs
@@ -0,0 +1,30 @@
+module Euclidean where
+
+import Algebra
+import Data.List
+
+newtype Poly = [Integer]
+
+addPoly :: Poly -> Poly -> Poly
+addPoly a b
+    | length a < length b = addPoly (a ++ [0]) b
+    | length a > length b = addPoly a (b ++ [0])
+    | otherwise = zipWith (+) a b
+
+multPoly :: Poly -> Poly -> Poly
+multPoly [] b = [] 
+multPoly a b = trunc (fmap (*(last a)) (take (length a - 1) (repeat 0) ++ b)) `addPoly` (multPoly ((init a)) b) where
+    trunc as
+        | as == [] = []
+        | last as == 0 = trunc $ init as
+        | otherwise = as
+
+instance Ring Poly where
+    rplus = addPoly
+    rplusid = []
+    rplusinv = map (*(-1))
+    rtimes = multPoly
+    rtimesid = [1]
+
+
+    
