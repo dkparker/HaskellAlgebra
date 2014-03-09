f0f20a88d7feb89a7af4a2198b48a3f7c0ec766c
Daniel Parker <dkparker@uchicago.edu>
2014-03-09 18:41:10 -0500

    Added ring instance for Euclidean.hs

--- Euclidean.hs
+++ Euclidean.hs
@@ -11,6 +11,12 @@ addPoly a b
     | length a > length b = addPoly a (b ++ [0])
     | otherwise = zipWith (+) a b
 
+subPoly :: Poly -> Poly -> Poly
+subPoly a b
+    | length a < length b = subPoly (a ++ [0]) b
+    | length a > length b = subPoly a (b ++ [0])
+    | otherwise = zipWith (-) a b
+
 multPoly :: Poly -> Poly -> Poly
 multPoly [] b = [] 
 multPoly a b = trunc (fmap (*(last a)) (take (length a - 1) (repeat 0) ++ b)) `addPoly` (multPoly ((init a)) b) where
@@ -18,6 +24,8 @@ multPoly a b = trunc (fmap (*(last a)) (take (length a - 1) (repeat 0) ++ b)) `a
         | as == [] = []
         | last as == 0 = trunc $ init as
         | otherwise = as
+
+--divPoly :: Poly -> Poly -> Poly
 
 instance Ring Poly where
     rplus = addPoly
